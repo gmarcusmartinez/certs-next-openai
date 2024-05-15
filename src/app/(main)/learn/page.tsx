d664e3956000d58d91926b82f5f491183cdb73fb
Marcus Martinez <gmarcusmartinez@gmail.com>
2024-05-15 18:09:10 +0200

    learn page layout

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,5 +1,26 @@
-import React from "react"
+import { FeedWrapper } from "@/components/feed-wrapper/FeedWrapper";
+import { LearnHeader } from "@/components/learn-header/LearnHeader";
+import { StickyWrapper } from "@/components/sticky-wrapper/StickyWrapper";
+import { UserProgress } from "@/components/user-progress/UserProgress";
+import React from "react";
 
 export default function LearnPage() {
-  return <div>page</div>
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{
+            title: "Spanish",
+            imageSrc: "/es.svg",
+          }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <LearnHeader title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
 }
