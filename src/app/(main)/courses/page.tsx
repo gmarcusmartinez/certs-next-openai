eed7102f7bb1622831b3e16f12b3d1f9986df186
Marcus Martinez <gmarcusmartinez@gmail.com>
2024-05-16 10:35:37 +0200

    revert database connection

---
+++ src/app/(main)/courses/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default async function CoursesPage() {
+  return (
+    <div className="mx-auto h-full max-w-[912px] px-3">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      {/* <List courses={courses} activeCourseId={userProgress?.activeCourseId} /> */}
+    </div>
+  );
+}
